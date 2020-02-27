cb7d7065d47d94d4d1e0a60e7d72a60559a2c608
skimanikings <kimanisimon856@gmail.com>
2020-02-27 14:56:53 +0300

    created a user and profile model and migrated them

--- bookhub/models.py
+++ bookhub/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField()
+    description = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+      
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField()
+    contact = models.CharField(max_length=80 ,null=True)
+    
+    
+    def __str__(self):
+            return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
